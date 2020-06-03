51724f6328e74de7886cabe9801dc317d2b9da56
sbm20202020 <starlymulebo2020@gmail.com>
2020-06-03 15:39:07 +0100

    Premier push

--- Backend/models.py
+++ Backend/models.py
@@ -28,11 +28,63 @@ class Adresse (ModelForTime):
 class MySchoolSchool(models.Model):
     adress=models.ForeignKey('Adresse', blank=True, null=True,on_delete=models.CASCADE, related_name="SchoolAdress_set")
     name = models.CharField(max_length=100, null=True, blank=True)
+
+#  # Debut de Code starly
+#     """ Cette variable identifie de maniere unique le detail d une ecole"""
+    details_school=models.OneToOneField('Details_school',on_delete=models.CASCADE,null=True,blank=True)
+    banque_liens_img_school=models.OneToOneField('Banque_liens_img_School',on_delete=models.CASCADE,null=True,blank=True)
+
     def __str__(self):
         return self.name
 
+# # Ici je creer un model qui contiendra tous les details de mon article --- Code starly
+class Details_school(models.Model):
+    name=models.CharField(max_length=50, null=True, blank=True)
+    historique= models.TextField(null=True,blank=True)
+    situation_geographique= models.TextField(null=True,blank=True)
+    impact_sociaux_eco= models.TextField(null=True,blank=True)
+    contacts=models.CharField(max_length=50, null=True, blank=True)
+    devise=models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Banque_liens_img_School(models.Model):
+    """docstring for Banque_liens_img_schools
+        cette classe nous permet de recuperer le path des images de base pour notre layout"""
+    lien= models.CharField(max_length=100, null=True, blank=True)
+    carousel= models.CharField(max_length=100, null=True, blank=True)
+    portfolio=models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        """ Tres utile lorsqu on veut faire un alt sur l image"""
+        # return "Image de : %s %s" % (MySchoolSchool.name, self.lien)
+        return self.lien
+
+class Activites_scolaire(models.Model):
+    """docstring for Activites_scolaires"""
+    titre_activite=models.CharField(max_length=100, null=True, blank=True)
+    description=models.TextField(null=True, blank=True)
+    lien_img=models.CharField(max_length=100, null=True, blank=True)
+    my_school_school=models.ForeignKey('MySchoolSchool', blank=True, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Activite : [ %s ] DE %s" % (self.titre_activite,self.my_school_school.name)
+
+class Options_organisee(models.Model):
+    """docstring for Options_organisees"""
+    titre_option=models.CharField(max_length=100, null=True, blank=True)
+    description=models.TextField(max_length=100,null=True, blank=True)
+    lien_img=models.CharField(max_length=100, null=True, blank=True)
+    my_school_school=models.ForeignKey('MySchoolSchool', blank=True, null=True,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Option : [ %s ] - [ %s ] DE %s" % (self.id,self.titre_option,self.my_school_school.name,)
+
+# Fin STARLY
+
 #ici le user doit d'abord faire partie de l'ecole après il peut etre assigner à un profil "RelationUserProfil"
-#parceque avant d'assigner 'un profil à un user, on doit d'abor selectionner tous les user lier à l'ecole 
+#parceque avant d'assigner 'un profil à un user, on doit d'abor selectionner tous les user lier à l'ecole
 class RelationSchoolUser(ModelForTime):
     school = models.ForeignKey('MySchoolSchool', blank=True, null=True,on_delete=models.CASCADE, related_name="MySchoolSchool_set")
     schooluser = models.ForeignKey('MySchoolUser', blank=True, null=True,on_delete=models.CASCADE, related_name="myschoolschoolSet_r")
